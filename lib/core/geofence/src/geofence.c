f7b89737f032a37d413ea8576e712d30be5df7eb
Sam <sam@bristolwireless.net>
2026-09-26 19:29:19 +0100

    lib: add core geofence point-in-polygon check

---
+++ lib/core/geofence/src/geofence.c
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2026 Agroecology Lab
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Ported from the public-domain/MIT PNPOLY algorithm by
+ * W. Randolph Franklin: https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
+ */
+
+#include <cerebri/core/geofence.h>
+
+bool geofence_point_in_polygon(const struct geofence_point *poly,
+				size_t n_verts,
+				struct geofence_point test)
+{
+	bool inside = false;
+
+	if ((poly != NULL) && (n_verts >= 3U)) {
+		size_t i;
+		size_t j = n_verts - 1U;
+
+		for (i = 0U; i < n_verts; i++) {
+			const bool y_straddles = ((poly[i].y > test.y) != (poly[j].y > test.y));
+
+			if (y_straddles) {
+				const double x_intersect = (poly[j].x - poly[i].x) *
+					(test.y - poly[i].y) / (poly[j].y - poly[i].y) +
+					poly[i].x;
+
+				if (test.x < x_intersect) {
+					inside = !inside;
+				}
+			}
+			j = i;
+		}
+	}
+
+	return inside;
+}
